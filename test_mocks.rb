2b7176803d0d6c2ac4f5e9e91df5ea7af49a41f7
Peter Law <PeterJCLaw@gmail.com>
2013-01-25 21:45:00 +0000

    Allow for other forms of test.
    Break our dummy PrivMessage into a base class and
    a couple of children that allow for different styles
    of test interrogation.
    
    Change-Id: I94fd616f02a63d10cfe66edddaa32daa512dcf85

--- test_mocks.rb
+++ test_mocks.rb
@@ -10,6 +10,25 @@ class PrivMessage
 	attr_accessor :message
 
 	def reply(thing)
+	end
+end
+
+class PrintingMessage < PrivMessage
+	def reply(thing)
 		print thing, "\n"
+	end
+end
+
+class TestableMessage < PrivMessage
+	def initialize
+		@replies = ""
+	end
+
+	def get_replies
+		return @replies
+	end
+
+	def reply(thing)
+		@replies += thing + "\n"
 	end
 end
